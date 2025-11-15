1dff32e8eda3e3103798c47a5602501edf50fae4
jawadmughal079 <mughaljawad12@gmail.com>
2025-11-15 10:25:13 +0500

    change th cors policy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 dotenv.config()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", 'https://book-store-frontend-pi-blush.vercel.app'],
     credentials: true
 }))
 
